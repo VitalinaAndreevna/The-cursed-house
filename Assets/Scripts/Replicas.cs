5f2048833bc670a12bb9fbd70a0dccb3859112ea
VitalinaAndreevna <vitalina.06.01.03@gmail.com>
2023-12-22 18:00:08 +0700

    Scene for game and text for replicas

---
+++ Assets/Scripts/Replicas.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Replicas : MonoBehaviour
+{
+    
+    public Canvas canvas;
+    public TextMeshProUGUI text;
+    static string[] TextRandom = 
+    {"Мой друг не мог покончить с собой! Я в это не верю! Нужно осмотреть весь дом и найти настоящую причину смерти",
+    "Я знал его слишком хорошо, чтобы поверить в самоубийство. Никаких проблем с законом, девушками или деньгами",
+    "Его звали ИМЯ - душа компании, любимец девушек и просто красавец. Он был одним из лучших SMM-менеджеров в городе",
+    "Боюсь, что мне не хватит оставшегося дня, чтобы прошерстить каждый уголок его особняка. Нужно остаться на ночь",
+    
+    "Чем же ты тут занимался, ИМЯ..."};
+    int TextRandomLength = TextRandom.Length;
+
+    static string[] TextHide =
+    {"Что за чёрт!? Что это было? Это не человек!",
+    "Кажется, я схожу с ума или это галлюцинации..."};
+    int TextHideLength = TextHide.Length;
+
+    static string[] TextInsidials =
+    {"Кажется, у ИМЯ были от меня секреты, рассказывать которые он не собирался",
+    "Как это связано с ИМЯ? Я пока не до конца понимаю",
+    "Что же это может значить?"};
+    int TextInsidialsLength = TextInsidials.Length;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Если время такое-то
+        if (Input.GetKeyDown(KeyCode.Alpha1)) //Это условие надо будет потом изменить
+        {
+            canvas.enabled = true;
+            text.text = TextRandom[(int)(Random.Range(0, TextRandomLength))];
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                canvas.enabled = false;
+            }
+        }
+
+        //Если статус игрока isHided = True
+        if (Input.GetKeyDown(KeyCode.Alpha2)) //Это условие надо будет потом изменить
+        {
+            canvas.enabled = true;
+            text.text = TextHide[(int)(Random.Range(0, TextHideLength))];
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                canvas.enabled = false;
+            }
+        }
+
+        //Если случилось сюжетное событие в виде поднятия предмета
+        if (Input.GetKeyDown(KeyCode.Alpha3)) //Это условие надо будет потом изменить
+        {
+            canvas.enabled = true;
+            text.text = TextInsidials[(int)(Random.Range(0, TextInsidialsLength))];
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                canvas.enabled = false;
+            }
+        }
+
+    }
+}
